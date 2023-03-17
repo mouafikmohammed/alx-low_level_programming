9df4fefb8327b24ced964343ba0bcf01befe0700
Mohammed Mouafik <mouafikmohammed03@gmail.com>
2023-03-16 19:01:49 -0700

    :)

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,8 +5,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	
+{	
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int len = sizeof(str) - 1;
 	write(2,str, len);
