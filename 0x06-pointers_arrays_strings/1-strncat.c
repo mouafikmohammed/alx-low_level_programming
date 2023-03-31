298549b0d87dee533cde6e6faa2437625dcffcd2
Mohammed Mouafik <mouafikmohammed03@gmail.com>
2023-03-30 19:34:07 -0700

    fix task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int index_dest, index_src;
 
-	for (index_dest = 0; dest[index_dest] != '\0'; i++)
+	for (index_dest = 0; dest[index_dest] != '\0'; index_dest++)
 	{
 		continue;
 	}
-	for (index_src = 0; src[index_src] != '\0' && index_src < n; j++)
+	for (index_src = 0; src[index_src] != '\0' && index_src < n; index_src++)
 	{
 		dest[index_dest + index_src] = src[index_src];
 	}
