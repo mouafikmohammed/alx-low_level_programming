99417ebf0c1615146bdaaad252a93f7b4f0b75d7
Mohammed Mouafik <mouafikmohammed03@gmail.com>
2023-03-30 19:31:01 -0700

    task 1 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @src: The source of strings
+ * @dest: The destination of the string
+ * @n: The length of int
+ *
+ * Return:Return a pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index_dest, index_src;
+
+	for (index_dest = 0; dest[index_dest] != '\0'; i++)
+	{
+		continue;
+	}
+	for (index_src = 0; src[index_src] != '\0' && index_src < n; j++)
+	{
+		dest[index_dest + index_src] = src[index_src];
+	}
+	dest[index_dest + index_src] = '\0';
+	return (dest);
+}
