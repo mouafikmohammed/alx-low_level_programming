946b7bcee0760bc9ea2461b8f430ca99e3749da8
mouafikmohammed <mouafikmohammed03@gmail.com>
2023-04-21 02:05:30 +0000

    tasks 0x10-variadic_functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef variadic_functions
+#define variadic_functions
+
+#include <stdarg.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif
